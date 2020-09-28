84607d4b75a802c2a967d0e9d07634c8489365ff
Lelihelija <lelihelija@gmail.com>
2020-09-28 17:54:05 +0300

    Updates

---
+++ precache-manifest.ee912f6e6833f6859d14bee52c14ba9e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "370277a30a62d0e943e1683c8b772af4",
+    "url": "/hw6/index.html"
+  },
+  {
+    "revision": "1fb955ab628a9b6e1c81",
+    "url": "/hw6/static/css/main.177f86ff.chunk.css"
+  },
+  {
+    "revision": "b1ed14254f17689050c0",
+    "url": "/hw6/static/js/2.259406e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hw6/static/js/2.259406e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fb955ab628a9b6e1c81",
+    "url": "/hw6/static/js/main.7e461bd6.chunk.js"
+  },
+  {
+    "revision": "b08d4a5ad6def9dc27ec",
+    "url": "/hw6/static/js/runtime-main.598ffb5b.js"
+  },
+  {
+    "revision": "3f90cae08f378df5d5eaea61fdf8a75c",
+    "url": "/hw6/static/media/1.3f90cae0.jpg"
+  },
+  {
+    "revision": "64d3323b131495e7e0e96405776608a2",
+    "url": "/hw6/static/media/2.64d3323b.jpg"
+  },
+  {
+    "revision": "8dc229c600a558b30cdce1f869a1377f",
+    "url": "/hw6/static/media/3.8dc229c6.jpg"
+  },
+  {
+    "revision": "73c9e9667d72f33becb533e50fe07704",
+    "url": "/hw6/static/media/4.73c9e966.jpg"
+  },
+  {
+    "revision": "2e0a702630c8987cc52de3fefb92ddc4",
+    "url": "/hw6/static/media/bokuto.2e0a7026.jpg"
+  },
+  {
+    "revision": "4ca10945a8c3eba4467f2f634e09aedf",
+    "url": "/hw6/static/media/chat.4ca10945.svg"
+  },
+  {
+    "revision": "2e61c3f50fa6a7a1abd423f0a196bc90",
+    "url": "/hw6/static/media/download.2e61c3f5.svg"
+  },
+  {
+    "revision": "aa7b03b3798417de76f0b62ab4d1c00e",
+    "url": "/hw6/static/media/heart.aa7b03b3.svg"
+  },
+  {
+    "revision": "60e3e4afe1ff85400e2e892c0e635cd3",
+    "url": "/hw6/static/media/kageyamaLogo.60e3e4af.jpg"
+  },
+  {
+    "revision": "049528162d54b9e6e1bb964ddb254841",
+    "url": "/hw6/static/media/kurooLogo.04952816.png"
+  },
+  {
+    "revision": "066d8846ccbbc22a75bafbf6c89a6a9b",
+    "url": "/hw6/static/media/reload.066d8846.svg"
+  }
+]);
